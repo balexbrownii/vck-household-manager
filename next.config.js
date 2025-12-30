b0dd2d9063333c0ea415924109e853f2b22890ef
Alex Brown <alex@example.com>
2025-12-30 11:09:07 -0500

    config: Simplify tsconfig to Next.js recommended settings

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   images: {
     remotePatterns: [],
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'app/'),
-    };
-    return config;
   },
 };
 
